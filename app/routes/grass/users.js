5515c1e9f3617366da6f101096e673f92fa924d0
jhassko1 <113586885+jhassko1@users.noreply.github.com>
2026-07-21 14:30:20 +0100

    Update users.js

--- app/routes/grass/users.js
+++ app/routes/grass/users.js
@@ -1479,7 +1479,7 @@ router.post("/golfbag", async (req, res) => {
                         table: table,
                         channel: "update",
                         oldDoc: item[0],
-                        newData: new_values.$set,
+                        newData: newvalues.$set,
                         user_id: item[0]._id,
                         user_email: email
                     }).catch(err => {
